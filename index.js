ef08c9d564b0e3bf79de81b5c09e3d192745a266
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2023-05-15 02:16:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,54 @@ const app = express();
 const { PORT, DATABASE_URL, SESSION_SECRET_KEY } = process.env;
 const expressLayouts = require('express-ejs-layouts');
 
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use(expressLayouts);
+
+// set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
+app.use(
+  session({
+    name: 'employee-review-system',
+    secret: SESSION_SECRET_KEY,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: MongoStore.create({
+      mongoUrl: DATABASE_URL,
+      autoRemove: 'disabled',
+    }),
+    function(err) {
+      console.log(err || 'connect-mongodb setup ok');
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// sets the authenticated user in the response
+app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
+
 // use express router
 app.use('/', require('./routes'));
 
