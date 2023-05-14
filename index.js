1feb831c0c1da424d5ee381626883706c104e60c
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2023-05-15 01:54:20 +0530

    First commit
    Server is up and running

---
+++ index.js
@@ -0,0 +1,17 @@
+const db=require('./config/database');
+// db();
+const express = require('express');
+const cookieParser = require('cookie-parser'); // parse cookie header and populate req.cookies
+const bodyParser = require('body-parser'); // parses incoming request bodies (req.body)
+const app = express();
+const { PORT, DATABASE_URL, SESSION_SECRET_KEY } = process.env;
+const expressLayouts = require('express-ejs-layouts');
+
+
+
+app.listen(PORT || 5000, (err) => {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
+  console.log(`Server is running on port: ${PORT}`);
+});
